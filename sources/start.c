ecc946f573d12e3a7c069431d405eee04cc5188b
Jennifer CHARLOIS <jcharloi@e2r10p10.42.fr>
2017-11-29 20:37:28 +0100

    c'est devenu organise

--- sources/start.c
+++ sources/start.c
@@ -6,7 +6,7 @@
 /*   By: jcharloi <jcharloi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 20:25:47 by jcharloi          #+#    #+#             */
-/*   Updated: 2017/11/28 19:57:01 by jcharloi         ###   ########.fr       */
+/*   Updated: 2017/11/29 20:29:23 by jcharloi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,29 @@ void		init_tab(t_pipe *pipe, t_ant *ant)
 	tabset(ant->tab, 0, i);
 }
 
-t_pipe		*new_path(t_global *global, t_ant *ant, char *str)
+t_pipe		*tube_get_marked(t_ant *ant, t_pipe *pipe, char *str)
 {
 	t_pipe	*tmp;
 
-	tmp = global->pipe;
+	tmp = pipe;
 	ant->i = 0;
-	ft_printf("str : %s\n", str);
+	while (tmp != NULL)
+	{
+		if ((ft_strcmp(tmp->s1, str) == 0 || ft_strcmp(tmp->s2, str) == 0) &&
+			ant->tab[ant->i] == 1)
+			return (tmp);
+		ant->i++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+
+t_pipe		*tube_get(t_ant *ant, t_pipe *pipe, char *str)
+{
+	t_pipe	*tmp;
+
+	tmp = pipe;
+	ant->i = 0;
 	while (tmp != NULL)
 	{
 		if ((ft_strcmp(tmp->s1, str) == 0 || ft_strcmp(tmp->s2, str) == 0) &&
@@ -44,7 +60,6 @@ t_pipe		*new_path(t_global *global, t_ant *ant, char *str)
 		ant->i++;
 		tmp = tmp->next;
 	}
-	ft_printf("NULL INCOMING\n");
 	return (NULL);
 }
 
@@ -52,37 +67,24 @@ void		start_algo(t_global *global, t_ant *ant)
 {
 	t_pipe	*tmp;
 	char	*here;
-	int		i;
 
-	i = 0;
 	init_tab(global->pipe, ant);
 	tmp = global->pipe;
-	while (tmp != NULL)
-	{
-		if (ft_strcmp(tmp->s1, ant->start) == 0 || ft_strcmp(tmp->s2, ant->start) == 0)
-			break ;
-		ant->i++;
-		tmp = tmp->next;
-	}
-	ft_printf("tmp->s1 = {%s} tmp->s2 = {%s}\n", tmp->s1, tmp->s2);
-	ant->tab[ant->i] = 1;
 	here = ant->start;
 	while (ft_strcmp(tmp->s1, ant->end) != 0 && ft_strcmp(tmp->s2, ant->end) != 0)
 	{
-		if (ft_strcmp(tmp->s1, here) == 0)
+		tmp = tube_get(ant, global->pipe, here);
+		if (tmp == NULL)
 		{
-			here = tmp->s2;
-			ft_printf("here 1: %s\n", here);
-			tmp = new_path(global, ant, tmp->s2);
+			tmp = tube_get_marked(ant, global->pipe, here);
+			ant->tab[ant->i] = 2;
 		}
 		else
-		{
+			ant->tab[ant->i] = 1;
+		if (ft_strcmp(tmp->s1, here) == 0)
+			here = tmp->s2;
+		else
 			here = tmp->s1;
-			ft_printf("here : %s\n", here);
-			tmp = new_path(global, ant, tmp->s1);
-		}
-		ant->tab[ant->i] = 1;
-		ft_printf("tmp->s1 = {%s} tmp->s2 = {%s}\n", tmp->s1, tmp->s2);
-		i++;
+		ft_printf("tmp->s1 = {%s} tmp->s2 = {%s} here : %s\n", tmp->s1, tmp->s2, here);
 	}
 }
